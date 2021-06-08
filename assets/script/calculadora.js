65ece5362125b9d0a6cee4a3f74f7299eaab372a
DaviPissinati <davi.pissinati@gmail.com>
2021-06-08 03:04:22 -0300

    [UPDATE]

---
+++ assets/script/calculadora.js
@@ -0,0 +1,106 @@
+// var $ = document.querySelector.bind(document);
+let $ = document.querySelector.bind(document);
+let resultado = $("#resultado");
+let n = 
+
+
+console.log(resultado);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var resultado = $("#resultado")
+
+// function escrever(valor){
+//     resultado.value += valor;
+// }
+ 
+// var n = $("btn");
+// n.onclick = function(){
+//     escrever('1')
+// }
+// var n = $(".n2");
+// n.onclick = function(){
+//     escrever('2')
+// }
+// var n = $(".n3");
+// n.onclick = function(){
+//     escrever('3')
+// }
+// var n = $(".n4");
+// n.onclick = function(){
+//     escrever('4')
+// }
+// var n = $(".n5");
+// n.onclick = function(){
+//     escrever('5')
+// }
+// var n = $(".n6");
+// n.onclick = function(){
+//     escrever('6')
+// }
+// var n = $(".n7");
+// n.onclick = function(){
+//     escrever('7')
+// }
+// var n = $(".n8");
+// n.onclick = function(){
+//     escrever('8')
+// }
+// var n = $(".n9");
+// n.onclick = function(){
+//     escrever('9')
+// }
+// var n1 = $(".n0");
+// n1.onclick = function(){
+//     escrever('0')
+// }
+
+// calculadora//
+//preto.onclick = function (){ toggleTheme('dark') };
+//var $ = document.querySelector.bind(document);
+// var preto = $(".preto");
+//     preto.onclick = function(){
+
+//         $("#corpo").style.background = 'black';   
+//         $("#texto").style.color = '#fff';
+//         $
+//         }
+// var branco = $(".branco");
+//     branco.onclick = function(){
+    
+//         $("#corpo").style.background = '#fff';   
+//         $("#texto").style.color = 'black';
+//     }
+
+// var $ = document.querySelector.bind(document);
+
+// var numeros = $(".n1");
+// var n1 = numeros.textContent;
+// console.log(n1);
+//     n1.onclick = function(){
+        
+//     }
